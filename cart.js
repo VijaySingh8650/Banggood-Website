404c8decad16ea800d4553bd96c2f3a263885619
Vijay_singh <rathorevijay995@gmail.com>
2022-05-06 16:10:33 +0530

    Cart filled

--- cart.js
+++ cart.js
@@ -1,47 +1,101 @@
 var cartData = JSON.parse(localStorage.getItem("cart"))|| [];
+
+var Email = JSON.parse(localStorage.getItem("login_data"));
 document.querySelector(".navbar2>img").addEventListener("click",function(){
     
     window.location.href="index.html";
 })
-if(cartData.length>0){
+if(Email.length>0){
+    document.querySelector(".navbar1>img+p").innerText="Hello, "+Email[0].email;
+    var sign = document.querySelector(".signOut");
+    sign.innerText="Sign Out";
+    sign.style.marginLeft="10px";
+    sign.style.cursor="pointer";
+    sign.addEventListener("click",function(){
+        window.location.href="login.html";
+    })
 
+}
+if(cartData.length>0){
+  
+ 
 
 displayOncart(cartData);
 function displayOncart(cartData){
-    
+    document.querySelector(".bag").innerHTML="";
     cartData.map(function(ele,index){
        var div = document.createElement("div");
        var image = document.createElement("img");
+       image.style.height="150px";
+       image.style.display="block";
+       image.style.margin="auto";
        image.setAttribute("src",ele.imageURL);
        var div1 =document.createElement("div");
        var price = document.createElement("p");
-       price.innerText=ele.price;
+       price.innerText="₹ "+ele.price;
+       
+       price.style.fontSize="14px";
+       price.style.color="#333";
+       price.style.fontWeight="700";
        var select= document.createElement("select");
-       var option1 = document.createElement("option");
-       option1.setAttribute("value","");
-       option1.innerText="Select Quantity";
+      
+       
        var option2 = document.createElement("option");
        option2.setAttribute("value",1);
        option2.innerText=1;
       
-     
+       select.style.width="40px";
+       
+       
        var option3 = document.createElement("option");
        option3.setAttribute("value",2);
        option3.innerText=2;
-       var option4 = document.createElement("option");
-       option4.setAttribute("value",3);
-       option4.innerText=3;
-       select.append(option1,option2,option3,option4);
-    //    select.addEventListener("change",function(){
-           
-    //    })
+       select.append(option2,option3);
+       select.addEventListener("change",function(){
+        var selected = select.value;
+        price.innerText="₹ "+(ele.price*selected);
+        
+       })
     div1.append(price,select);
-    div.append(image,div1);
-    document.querySelector(".bag").append(div);
-
+    div1.style.margin="10px 0";
+    div1.style.display="flex";
+    div1.style.justifyContent="space-between";
+    var btn = document.createElement("button");
+    btn.innerText = "Delete";
+    btn.style.background="none";
+    btn.style.border="none";
+    btn.style.color="white";
+    btn.style.padding="5px 10px 5px 10px";
+    btn.style.borderRadius="2px";
+    btn.style.fontSize="12px";
+    btn.style.cursor="pointer";
+    btn.style.background="#ff4733";
+    btn.addEventListener("click",function(){
+        del(ele,index);
+    })
+    div.append(image,div1,btn);
+    div.style.margin="20px 10px 20px 10px";
+    div.style.background="#ebedf0";
+    div.style.padding="10px 10px 10px 10px";
+    var bag = document.querySelector(".bag");
+    bag.append(div);
+    bag.style.width="100%";
+    bag.style.display="grid";
+    bag.style.gridTemplateColumns="repeat(6, 1fr)";
+    bag.style.gap="10px";
+    bag.style.background="white";
+    bag.style.marginTop="50px";
     }) 
+    
+}
+function del(ele,index){
+    cartData.splice(index,1);
+    localStorage.setItem("cart",JSON.stringify(cartData));
+   
+    displayOncart(cartData);
 }
 }
+
 else{
     var image = document.createElement("img");
     image.setAttribute("src","Images Index/sprite2.png");
@@ -88,3 +142,4 @@ else{
     bag.style.alignItems="center";
      
 }
+
