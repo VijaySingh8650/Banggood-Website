66443386ad3173da960fb8598c726d3edbb0611d
abhijeetdagade <101392865+abhijeetdagade@users.noreply.github.com>
2022-05-09 08:06:38 +0530

    Add files via upload

---
+++ women's.js
@@ -0,0 +1,93 @@
+var data=[{img:"https://imgaz3.staticbg.com/thumb/view/oaupload/banggood/images/BD/1E/5fcd2c9b-c966-4972-ae4d-caa0a2f4d59d.jpg.webp",price:"₹1599",strikePrice:"₹2099",offPercentage:" 53% Off",about:"African Hallow Braided circle",id:1,q:1},
+{img:"https://imgaz.staticbg.com/thumb/view/oaupload/banggood/images/FB/4B/4d9a1e5b-5bde-4d16-afbe-24c48612740a.jpeg.webp",price:"₹1768",strikePrice:"₹2499",offPercentage:" 49% Off",about:"925 strailing silvr",id:2,q:1},
+{img:"https://imgaz3.staticbg.com/thumb/view/oaupload/banggood/images/CB/06/22bab4f1-704e-44b9-9432-3e0088ed5896.jpg.webp",price:"₹1311",strikePrice:"₹1999",offPercentage:" 34% Off",about:"bohimen creative luminious",id:3,q:1},
+{img:"https://imgaz3.staticbg.com/thumb/view/oaupload/banggood/images/8B/72/a23e4914-54ae-4556-aa59-cdf85a812ab2.jpg.webp",price:"₹1256",strikePrice:"₹2999",offPercentage:" 39% Off",about:"silver plated simple snake",id:4,q:1},
+{img:"https://imgaz.staticbg.com/thumb/view/oaupload/banggood/images/4E/32/be1531f2-d0b3-4c30-9e5c-5e15cb9da40a.jpg.webp",price:"₹3167",strikePrice:"₹4999",offPercentage:" 66% Off",about:"silver plated simple glass slipper",id:5,q:1},
+{img:"https://imgaz3.staticbg.com/thumb/view/oaupload/banggood/images/B2/1C/6609d3a6-d2ac-4351-af0f-2aaee8ab8249.jpg.webp",price:"₹1229",strikePrice:"₹3499",offPercentage:" 60% Off",about:"multi color glass sliper slipper",id:6,q:1},
+{img:"https://imgaz1.staticbg.com/thumb/view/oaupload/banggood/images/B3/F8/0c15e924-b9b9-4d20-8df7-21c585f462bc.jpg.webp",price:"₹1699",strikePrice:"₹2099",offPercentage:" 19% Off",about:"mono color glass magnifire",id:7,q:1},
+{img:"https://imgaz3.staticbg.com/thumb/view/oaupload/banggood/images/D1/74/6d5ad3f3-9a2b-4e97-9745-05bc6e25d0af.jpg.webp",price:"₹4445",strikePrice:"₹5399",offPercentage:" 18% Off",about:"punk genuine leather warp",id:8,q:1},
+{img:"https://imgaz2.staticbg.com/thumb/view/oaupload/banggood/images/58/AB/34538bae-f75b-48c6-ba16-498041157c16.jpg.webp",price:"₹1074",strikePrice:"₹1699",offPercentage:" 37% Off",about:"tanzanite ziracvon loose",id:9,q:1},
+{img:"https://imgaz2.staticbg.com/thumb/view/oaupload/ser1/banggood/images/AC/E6/c1634b4e-2179-4850-8b61-52759a9fb856.jpg.webp",price:"₹1297",strikePrice:"₹2099",offPercentage:" 38% Off",about:"double eagle",id:10,q:1},
+{img:"https://imgaz2.staticbg.com/thumb/view/oaupload/banggood/images/49/2D/8383af63-c90b-40e5-9a77-3b9393d84d93.png.webp",price:"₹736",strikePrice:"1099",offPercentage:" 33% Off",about:"i love you present",id:11,q:1},
+{img:"https://imgaz1.staticbg.com/thumb/view/oaupload/banggood/images/B6/A2/bb0f6129-0fe2-47b1-bf40-cef77f6c7ba0.jpg.webp",price:"₹1199",strikePrice:"₹1499",offPercentage:" 20% Off",about:"gold plated peremeter cristal",id:12,q:1},
+{img:"https://imgaz1.staticbg.com/thumb/view/oaupload/banggood/images/9E/92/2c8c8c4b-d5b4-48b5-99a7-3a7b46d9362f.jpg.webp",price:"₹2388",strikePrice:"₹3499",offPercentage:" 32% Off",about:"diamond tester gemstone selecter",id:13,q:1},
+{img:"https://imgaz2.staticbg.com/thumb/view/oaupload/banggood/images/A3/D2/e9cbc051-fc2f-4821-9a46-de3d4fd0339d.jpg.webp",price:"₹588",strikePrice:"₹899",offPercentage:" 35% Off",about:"vintage multilayer braceler",id:14,q:1},
+{img:"https://imgaz3.staticbg.com/thumb/view/oaupload/banggood/images/33/C1/f88d4f18-1a2a-42ea-bcc0-b62b8158876c.jpg.webp",price:"₹660",strikePrice:"₹1199",offPercentage:" 45% Off",about:"hope racin geometry",id:15,q:1},
+{img:"https://imgaz2.staticbg.com/thumb/view/oaupload/banggood/images/39/72/391e5b75-658d-4bdf-8d25-3edcafbba3df.jpg.webp",price:"₹1311",strikePrice:"₹2099",offPercentage:" 38% Off",about:"special gircon inland",id:16,q:1},
+{img:"https://imgaz3.staticbg.com/thumb/view/oaupload/banggood/images/73/E4/64b8047f-f40a-43ca-89c8-8caa58f7dce4.jpg.webp",price:"₹957",strikePrice:"₹1699",offPercentage:" 44% Off",about:"cute pendant neckless",id:17,q:1},
+{img:"https://imgaz.staticbg.com/thumb/view/oaupload/banggood/images/B3/52/300367a2-4f33-4cb7-a1e4-42776017e105.jpg.webp",price:"₹1364",strikePrice:"₹1999",offPercentage:" 32% Off",about:"gold plated 10mm plane chain",id:18,q:1},
+{img:"https://imgaz1.staticbg.com/thumb/view/oaupload/banggood/images/DA/54/67c75fe6-9ca1-487b-ae19-27fbb1705c25.jpg.webp",price:"₹1429",strikePrice:"₹2099",offPercentage:" 32% Off",about:"cristal rehiness stone",id:19,q:1},
+{img:"https://imgaz1.staticbg.com/thumb/view/oaupload/banggood/images/5B/BE/92f5a9b1-5c79-43c5-a9b8-bdb9a0c24f36.jpg.webp",price:"₹704",strikePrice:"₹2299",offPercentage:" 69% Off",about:"luminus gold silver dragon",id:20,q:1},
+{img:"https://imgaz3.staticbg.com/thumb/view/oaupload/banggood/images/36/3C/50f5d169-1495-49e2-b9c8-5b6ef63e6d50.jpg.webp",price:"₹1194",strikePrice:"₹1799",offPercentage:" 38% Off",about:"trendy zericate zirrconia crystal",id:21,q:1},
+{img:"https://imgaz3.staticbg.com/thumb/view/oaupload/banggood/images/12/2C/8091a30d-3afb-498e-8597-425ffbfc83a4.jpg.webp",price:"₹1582",strikePrice:"₹2199",offPercentage:" 28% Off",about:"elagent silver rehine stone",id:22,q:1},
+{img:"https://imgaz2.staticbg.com/thumb/view/oaupload/banggood/images/CA/21/b1d050c4-6b90-41c6-9b68-5cfdc7e1c0ad.jpg.webp",price:"₹514",strikePrice:"₹1099",offPercentage:" 53% Off",about:"African Hallow Braided circle",id:23,q:1},
+{img:"https://imgaz2.staticbg.com/thumb/view/oaupload/banggood/images/3C/54/4ad8dca9-4dbc-4f46-9d6e-2e01045a9c34.jpg.webp",price:"₹768",strikePrice:"₹1499",offPercentage:" 49% Off",about:"925 strailing silvr",id:24,q:1},
+{img:"https://imgaz3.staticbg.com/thumb/view/oaupload/banggood/images/44/81/d51d0900-9842-45a3-91c2-b4fda9454b32.jpg.webp",price:"₹1311",strikePrice:"₹1999",offPercentage:" 34% Off",about:"bohimen creative luminious",id:25,q:1},
+{img:"https://imgaz.staticbg.com/thumb/view/oaupload/banggood/images/88/B7/cc272112-c122-4670-88e5-15c0a6c825e4.jpg.webp",price:"₹1256",strikePrice:"₹1999",offPercentage:" 39% Off",about:"silver plated simple snake",id:26,q:1},
+{img:"https://imgaz3.staticbg.com/thumb/view/oaupload/banggood/images/92/6D/3efd74e1-fd88-4cf0-8fc9-58383b129983.jpg.webp",price:"₹367",strikePrice:"₹999",offPercentage:" 66% Off",about:"silver plated simple glass slipper",id:27,q:1},
+{img:"https://imgaz1.staticbg.com/thumb/view/oaupload/banggood/images/B0/C8/f4873f35-f9b8-4a90-9be2-699074bbaa62.jpg.webp",price:"₹229",strikePrice:"₹899",offPercentage:" 60% Off",about:"multi color glass sliper slipper",id:28,q:1},
+{img:"https://imgaz2.staticbg.com/thumb/view/oaupload/banggood/images/89/8D/ac10e317-52fe-45d5-a665-5bed2cd23773.jpg.webp",price:"₹1699",strikePrice:"₹2099",offPercentage:" 19% Off",about:"mono color glass magnifire",id:29,q:1},
+{img:"https://imgaz2.staticbg.com/thumb/view/oaupload/banggood/images/C0/E7/87ea06e6-da80-4645-8261-893b942201c0.jpg.webp",price:"₹4445",strikePrice:"₹5399",offPercentage:" 18% Off",about:"punk genuine leather warp",id:30,q:1},
+{img:"https://imgaz2.staticbg.com/thumb/view/oaupload/banggood/images/C0/E7/87ea06e6-da80-4645-8261-893b942201c0.jpg.webp",price:"₹1074",strikePrice:"₹1699",offPercentage:" 37% Off",about:"tanzanite ziracvon loose",id:31,q:1},
+{img:"https://imgaz.staticbg.com/thumb/view/oaupload/banggood/images/9E/5A/80dd79c6-aa91-4979-8d2f-a7d550fcbccd.jpg.webp",price:"₹1297",strikePrice:"₹2099",offPercentage:" 38% Off",about:"double eagle",id:32,q:1},
+{img:"https://imgaz2.staticbg.com/thumb/view/oaupload/banggood/images/42/41/43163949-a257-443c-b442-6d50c87e1259.jpg.webp",price:"₹736",strikePrice:"₹1099",offPercentage:" 33% Off",about:"i love you present",id:33,q:1},
+{img:"https://imgaz2.staticbg.com/thumb/view/oaupload/banggood/images/84/37/ef8b81ca-25fe-4151-9f07-b4b0ba5313a5.jpg.webp",price:"₹1199",strikePrice:"₹1499",offPercentage:" 20% Off",about:"gold plated peremeter cristal",id:34,q:1},
+{img:"https://imgaz2.staticbg.com/thumb/view/oaupload/banggood/images/34/EC/9a679bb6-2121-4f38-b539-0f97420fbbf9.jpg.webp",price:"₹2388",strikePrice:"₹3499",offPercentage:" 32% Off",about:"diamond tester gemstone selecter",id:35,q:1},
+{img:"https://imgaz2.staticbg.com/thumb/view/oaupload/banggood/images/08/37/ecbbc379-a56c-4c44-88e8-a7d3fcae7a31.jpg.webp",price:"₹588",strikePrice:"₹899",offPercentage:" 35% Off",about:"vintage multilayer braceler",id:36,q:1},
+{img:"https://imgaz3.staticbg.com/thumb/view/oaupload/banggood/images/C9/9E/f00f550a-ebf9-40f9-97ea-1ce6e2ccafa4.jpg.webp",price:"₹660",strikePrice:"₹1199",offPercentage:" 45% Off",about:"hope racin geometry",id:37,q:1},
+{img:"https://imgaz2.staticbg.com/thumb/view/oaupload/banggood/images/6B/EC/d5a6fd27-4582-4911-b0fb-332e46490ce2.jpg.webp",price:"₹1311",strikePrice:"₹2099",offPercentage:" 38% Off",about:"special gircon inland",id:38,q:1},
+{img:"https://imgaz.staticbg.com/thumb/view/oaupload/banggood/images/1A/46/7b6c6d8d-0ed3-402c-b5e4-95d5c4477d5d.jpg.webp",price:"₹957",strikePrice:"₹1699",offPercentage:" 44% Off",about:"cute pendant neckless",id:39,q:1},
+{img:"https://imgaz.staticbg.com/thumb/view/oaupload/banggood/images/27/D7/53c491bd-6bb9-4d27-9c44-d012e098ecd7.jpg.webp",price:"₹1364",strikePrice:"₹1999",offPercentage:" 32% Off",about:"gold plated 10mm plane chain",id:40,q:1},
+{img:"https://imgaz3.staticbg.com/thumb/view/oaupload/banggood/images/B8/81/82996695-05ab-46e8-8542-a535741915ce.jpg.webp",price:"₹1429",strikePrice:"₹2099",offPercentage:" 32% Off",about:"cristal rehiness stone",id:41,q:1},
+{img:"https://imgaz.staticbg.com/thumb/view/oaupload/banggood/images/E8/F2/7b9ff7e7-1e67-4664-bd5e-bddbbbc479a8.jpg.webp",price:"₹704",strikePrice:"₹2299",offPercentage:" 69% Off",about:"luminus gold silver dragon",id:42,q:1},
+{img:"https://imgaz1.staticbg.com/thumb/view/oaupload/banggood/images/30/2E/e43d8d76-c280-4b23-a21e-f825e661358e.jpg.webp",price:"₹1194",strikePrice:"₹1799",offPercentage:" 38% Off",about:"trendy zericate zirrconia crystal",id:43,q:1},
+{img:"https://imgaz2.staticbg.com/thumb/view/oaupload/banggood/images/83/3E/c2f8f2c7-d137-46ae-b76e-82346864b983.jpg.webp",price:"₹1582",strikePrice:"₹2199",offPercentage:" 28% Off",about:"elagent silver rehine stone",id:44,q:1},
+{img:"https://imgaz.staticbg.com/thumb/view/oaupload/banggood/images/48/EA/41775aca-d4cf-4532-b0b0-0c10411a21c7.jpg.webp",price:"₹514",strikePrice:"₹1099",offPercentage:" 53% Off",about:"African Hallow Braided circle",id:45,q:1},
+{img:"https://imgaz2.staticbg.com/thumb/view/oaupload/banggood/images/2C/78/cfa237f0-27b1-4a03-9e7d-cf0195048925.jpg.webp",price:"₹768",strikePrice:"₹1499",offPercentage:" 49% Off",about:"925 strailing silvr",id:46,q:1},
+{img:"https://imgaz1.staticbg.com/thumb/view/oaupload/ser1/banggood/images/69/B3/576408d3-9641-4804-9983-9ec1cc092b66.jpg.webp",price:"₹1311",strikePrice:"₹1999",offPercentage:" 34% Off",about:"bohimen creative luminious",id:47,q:1},
+{img:"https://imgaz.staticbg.com/thumb/view/oaupload/banggood/images/91/AB/d9ec6bf3-b224-4a7d-bab7-bfa37bdde83e.jpg.webp",price:"₹1256",strikePrice:"₹1999",offPercentage:" 39% Off",about:"silver plated simple snake",id:48,q:1},
+{img:"https://imgaz2.staticbg.com/thumb/view/oaupload/banggood/images/61/42/4ffeb655-d861-4259-9bdb-483fd24bb9fd.jpeg.webp",price:"₹367",strikePrice:"₹999",offPercentage:" 66% Off",about:"silver plated simple glass slipper",id:49,q:1},
+{img:"https://imgaz3.staticbg.com/thumb/view/oaupload/banggood/images/A9/20/d3a60a7e-2c81-40e8-b6b8-b3d90f891aea.jpg.webp",price:"₹229",strikePrice:"₹899",offPercentage:" 60% Off",about:"multi color glass sliper slipper",id:50,q:1},
+{img:"https://imgaz.staticbg.com/thumb/view/oaupload/banggood/images/D8/48/5388d425-4972-4015-951f-3f89f723db39.jpg.webp",price:"₹1699",strikePrice:"₹2099",offPercentage:" 19% Off",about:"mono color glass magnifire",id:51,q:1},
+{img:"https://imgaz2.staticbg.com/thumb/view/oaupload/banggood/images/8F/24/9223c1c9-08a1-464c-b035-19de38e25d8e.jpg.webp",price:"₹4445",strikePrice:"₹5399",offPercentage:" 18% Off",about:"punk genuine leather warp",id:52,q:1},
+{img:"https://imgaz3.staticbg.com/thumb/view/oaupload/banggood/images/23/02/9b85d760-1725-4221-9d13-7b0f51de4bfc.jpg.webp",price:"₹1074",strikePrice:"₹1699",offPercentage:" 37% Off",about:"tanzanite ziracvon loose",id:53,q:1},
+{img:"https://imgaz2.staticbg.com/thumb/view/oaupload/banggood/images/5B/20/afb5d920-eae3-4230-94a7-5541770e80b1.jpg.webp",price:"₹1297",strikePrice:"₹2099",offPercentage:" 38% Off",about:"double eagle",id:54,q:1},
+{img:"https://imgaz3.staticbg.com/thumb/view/oaupload/banggood/images/BD/29/02db7d81-35b7-4d9c-a53d-f36bbef3a738.jpg.webp",price:"₹736",strikePrice:"₹1099",offPercentage:" 33% Off",about:"i love you present",id:55,q:1},
+{img:"https://imgaz3.staticbg.com/thumb/view/oaupload/banggood/images/D4/8E/e63e0127-7bd8-4260-9b1f-5463cf171995.jpg.webp",price:"₹1199",strikePrice:"₹1499",offPercentage:" 20% Off",about:"gold plated peremeter cristal",id:56,q:1},
+{img:"https://imgaz2.staticbg.com/thumb/view/oaupload/banggood/images/23/88/4cfc49a5-2d52-4c9d-b416-13c88395b342.jpg.webp",price:"₹2388",strikePrice:"₹3499",offPercentage:" 32% Off",about:"diamond tester gemstone selecter",id:57,q:1},
+{img:"https://imgaz1.staticbg.com/thumb/view/oaupload/banggood/images/FC/87/184b1f39-2209-4a3b-a775-b9bfbe859779.jpg.webp",price:"₹588",strikePrice:"₹899",offPercentage:" 35% Off",about:"vintage multilayer braceler",id:58,q:1},
+{img:"https://imgaz3.staticbg.com/thumb/view/oaupload/ser1/banggood/images/B3/BF/a23d55da-0963-494a-8934-1f08862e1a02.jpg.webp",price:"₹660",strikePrice:"₹1199",offPercentage:" 45% Off",about:"hope racin geometry",id:59,q:1},
+{img:"https://imgaz3.staticbg.com/thumb/view/oaupload/banggood/images/62/4A/fbe7bf4f-758d-4fe2-8e09-db6f1c50a60e.jpg.webp",price:"₹1311",strikePrice:"₹2099",offPercentage:" 38% Off",about:"special gircon inland",id:60,q:1},
+{img:"https://imgaz1.staticbg.com/thumb/view/oaupload/ser1/banggood/images/3F/8B/fb9578da-726e-444e-85d3-b8fa2bfeb3db.jpg.webp",price:"₹957",strikePrice:"₹1699",offPercentage:" 44% Off",about:"cute pendant neckless",id:61,q:1},
+{img:"https://imgaz3.staticbg.com/thumb/view/oaupload/banggood/images/C8/42/281d84eb-1433-4756-b280-eade9869a07b.jpg.webp",price:"₹1364",strikePrice:"₹1999",offPercentage:" 32% Off",about:"gold plated 10mm plane chain",id:62,q:1},
+{img:"https://imgaz1.staticbg.com/thumb/view/oaupload/banggood/images/8F/6B/164f59cc-9979-4ce7-9cfe-7156ed43e650.jpg.webp",price:"₹1429",strikePrice:"₹2099",offPercentage:" 32% Off",about:"cristal rehiness stone",id:63,q:1},
+{img:"https://imgaz3.staticbg.com/thumb/view/oaupload/banggood/images/B3/ED/9f729797-f11e-48be-9015-08a923d7a3d7.jpg.webp",price:"₹704",strikePrice:"₹2299",offPercentage:" 69% Off",about:"luminus gold silver dragon",id:64,q:1},
+{img:"https://imgaz.staticbg.com/thumb/view/oaupload/banggood/images/38/2B/33bfe519-2842-4443-9022-c4b4c29517dd.jpg.webp",price:"₹1194",strikePrice:"₹1799",offPercentage:" 38% Off",about:"trendy zericate zirrconia crystal",id:65,q:1},
+]
+
+display(data);
+function display(data){
+    var parentOfCard=document.getElementById("jewelryTopRightBottom");
+    parentOfCard.innerHTML="";
+
+    data.forEach(function(el){
+        var card=document.createElement("div");
+        var img=document.createElement("img");
+        img.setAttribute("src",el.img);
+        var price=document.createElement("h2");
+        price.innerText=el.price;
+        var para=document.createElement("p");
+        para.innerHTML="<span>"+el.strikePrice+"</span> "+el.offPercentage+"";
+        // var strikePrice=document.createElement("span");
+        // strikePrice.innerText=el.strikePrice;
+        // para.append(strikePrice);
+        // para.innerText=el.offPercentage;
+        var about=document.createElement("a");
+        about.innerText=el.about;
+        var review=document.createElement("a");
+        review.innerText="review";
+        card.append(img,price,para,about,review);
+
+        parentOfCard.append(card);
+    });
+}
